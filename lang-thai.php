081d9e98249ea246f8d2907e6b55c90de2aafd22
Grid Kr <gridkr@gmail.com>
2014-08-17 19:04:30 +0700

    Create lang-thai.php

---
+++ lang-thai.php
@@ -0,0 +1,40 @@
+<?php
+/**************************************************************************/
+/* AppServ Language: The Future of the Web                                */
+/**************************************************************************/
+define("_CHARSET","utf-8");
+define("_A","เกี่ยวกับ<!--โปรแกรม-->");
+define("_FOR","สำหรับ");
+define("_IS","คือ โปรแกรมที่รวบรวมโอเพ่นซอร์สซอฟท์แวร์หลายๆ อย่างเข้าด้วยกัน โดยมี :");
+define("_APACHE","Apache Web Server");
+define("_MYSQL","MySQL Database");
+define("_FAHTHAI","ฟ้า");
+define("_INTHTHAI","ไทย");
+define("_PERL","Perl");
+define("_GRIDDOTKIM","กฤต ดอท<b>คิม</b>");
+define("_PHP","PHP Script Language");
+define("_PHPINFO","PHP Information");
+define("_ZENDOPT","Zend Optimizer");
+define("_PHPMYADMIN","phpMyAdmin Database Manager");
+define("_PHPNUKE","พีเอชพีนู้ค เว็บสำเร็จรูป");
+define("_PHPBB","พีเอชพีบีบี เว็บบอร์ด");
+define("_VERSION","เวอร์ชั่น");
+define("_CHANGELOG","มีอะไร<B>ใหม่</b>");
+define("_README","อ่านฉัน (Read Me)");
+define("_AUTHOR","เกี่ยวกับผู้จัดทำ");
+define("_COPYING","เกี่ยวกับลิกขะฉิด");
+define("_SITE","โฮมเพจ");
+define("_LANG","เลือกภาษา");
+define("_S","<!-- สร้างเว็บเซิร์ฟเวอร์, ดาต้าเบสเซิร์ฟเวอร์ง่ายๆ ด้วย AppServ -->:-) ");
+define("_QWERTYUIOP","สร้างเว็บเซิร์ฟเวอร์, ดาต้าเบสเซิร์ฟเวอร์ง่ายๆ ด้วย AppServ :-) ");
+define("_OS","วินโดวส์");
+define("_OFSITE","เว็บไซต์หลัก");
+define("_HSUP","ผู้ให้การสนับสนุนเว็บโฮสติ้ง");
+define("_W","วิกิพีเดียไทย - ปรับปรุงล่าสุด");
+define("_D","วิกิพจนานุกรมไทย - ปรับปรุงล่าสุด");
+define("_Q","วิกิคำคมไทย - ปรับปรุงล่าสุด");
+define("_R","ไร้สาระนุกรมไทย - ปรับปรุงล่าสุด");
+define("_K","คือ");
+define("_L","ล่าสุด");
+define("_N","ศูนย์เตือนภัยพิบัติแห่งชาติ"); 
+?>
